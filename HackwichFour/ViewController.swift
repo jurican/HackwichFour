d155a72ee2742f9437def1e42a16f4ae280e07f5
jurican <97710250+jurican@users.noreply.github.com>
2022-02-23 21:58:15 -1000

    My bad attempt doing the IBOutlets

--- HackwichFour/ViewController.swift
+++ HackwichFour/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstTabLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    self.firstTabLable.text = "About"
+        
     }
-
+   
 
 }
 
